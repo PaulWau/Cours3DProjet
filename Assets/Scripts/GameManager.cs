4d991c7372820d7bf2f791f542079b5dd95b1be4
TeoGras <teo.gras@student.junia.fr>
2022-02-27 21:56:12 +0100

    PartieGraphique

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,7 @@ public class GameManager : MonoBehaviour
 {
     //Settings variables
     public static GameObject settingsMenu;
+    public static GameObject playerInterface;
     public static int sensiValue;
     public static int sensiMax = 1000;
     public static int sensiMin = 50;
@@ -16,8 +17,13 @@ public class GameManager : MonoBehaviour
     public static int audioVolumMin = 0;
     
     //Player variables
-    public static int playerLife;
-    public static int playerAmmo;
+    public static int playerLife = 100;
+    public static int playerLifeMax = 100;
+    public static int playerLifeMin = 0;
+    
+    public static int playerAmmo = 60;
+    public static int playerAmmoMax = 60;
+    public static int playerAmmoMin = 0;
 
 
     private void Start()
@@ -25,9 +31,88 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(this);
         settingsMenu = Instantiate(Resources.Load("SettingsMenu") as GameObject);
         settingsMenu.SetActive(false);
+        
+        playerInterface = Instantiate(Resources.Load("PlayerInterface") as GameObject);
+        playerInterface.SetActive(false);
+        
         DontDestroyOnLoad(settingsMenu);
+        DontDestroyOnLoad(playerInterface);
         sensiValue = 100;
         musicVolume = 100;
         soundVolume = 100;
+
+        playerInterface.GetComponent<IndicatorReference>().LifeIndicator.text = playerLife.ToString();
+        playerInterface.GetComponent<IndicatorReference>().AmmoIndicator.text = playerAmmo.ToString();
     }
+
+    public static void TakeDamage(int damageTaken)
+    {
+        if (playerLife - damageTaken <= playerLifeMin)
+        {
+            playerLife = playerLifeMin;
+        }
+        else if (playerLife - damageTaken >= playerLifeMax)
+        {
+            playerLife = playerLifeMax;
+        }
+        else
+        {
+            playerLife = playerLife - damageTaken;
+        }
+        
+        playerInterface.GetComponent<IndicatorReference>().LifeIndicator.text = playerLife.ToString();
+    }
+
+    public static void SetPlayerLife(int life)
+    {
+        if (life<=playerLifeMin)
+        {
+            playerLife = playerLifeMin;
+        }
+        else if (life>=playerLifeMax)
+        {
+            playerLife = playerLifeMax;
+        }
+        else
+        {
+            playerLife = life;
+        }
+        playerInterface.GetComponent<IndicatorReference>().LifeIndicator.text = playerLife.ToString();
+    }
+    
+    public static void DecreaseAmmo(int ammoToDecrease)
+    {
+        if (playerAmmo - ammoToDecrease <= playerAmmoMin)
+        {
+            playerAmmo = playerLifeMin;
+        }
+        else if (playerLife - ammoToDecrease >= playerAmmoMax)
+        {
+            playerAmmo = playerLifeMax;
+        }
+        else
+        {
+            playerAmmo = playerAmmo - ammoToDecrease;
+        }
+        
+        playerInterface.GetComponent<IndicatorReference>().AmmoIndicator.text = playerAmmo.ToString();
+    }
+
+    public static void SetAmmo(int ammo)
+    {
+        if (ammo<=playerAmmoMin)
+        {
+            playerAmmo = playerAmmoMin;
+        }
+        else if (ammo>=playerAmmoMax)
+        {
+            playerAmmo = playerAmmoMax;
+        }
+        else
+        {
+            playerAmmo = ammo;
+        }
+        playerInterface.GetComponent<IndicatorReference>().LifeIndicator.text = playerLife.ToString();
+    }
+    
 }
---
+++ Assets/Scripts/IndicatorReference.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TMPro;
+using UnityEngine;
+
+public class IndicatorReference : MonoBehaviour
+{
+    public TMP_Text LifeIndicator;
+    public TMP_Text AmmoIndicator;
+}
