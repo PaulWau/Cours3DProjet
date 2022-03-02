b31b5be16b2c17be32d554e17414ab33df848809
LouisDsr <deseure.louis@gmail.com>
2022-03-02 11:28:39 +0100

    son shoot +reload

--- Assets/Scripts/SceneLouisScripts/ShootScript.cs
+++ Assets/Scripts/SceneLouisScripts/ShootScript.cs
@@ -7,14 +7,23 @@ public class ShootScript : MonoBehaviour
 {
     [SerializeField] Rigidbody projectile;
     [SerializeField] float speed = 100;
+    public AudioSource shootSound;
+    public AudioSource reloadSound;
     //[SerializeField] private Transform bulletHolder;
     
     void Update()
+    {
+        Shoot();
+        ReloadAmmo();
+    }
+
+    private void Shoot()
     {
         if (GameManager.playerAmmo > 0)
         {
             if (Input.GetButtonDown("Fire1"))
             {
+                shootSound.Play();
                 Rigidbody p = Instantiate(projectile, transform.position + transform.right * 0.8f, Quaternion.identity);
                 //p.transform.SetParent(bulletHolder);
                 p.velocity = transform.right * speed;
@@ -22,6 +31,13 @@ public class ShootScript : MonoBehaviour
                 GameManager.playerAmmo -= 1;
             }
         }
-            
+    }
+    private void ReloadAmmo()
+    {
+        if (Input.GetButton("Reload"))
+        {
+            reloadSound.Play();
+            GameManager.playerAmmo = GameManager.playerAmmoMax;
+        }
     }
 }
